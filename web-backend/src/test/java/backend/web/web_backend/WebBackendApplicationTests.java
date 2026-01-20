e545f1785e0a6ce817d5e40e5c9490c41668fd8e
nathan77777 <diaynathan@gmail.com>
2026-01-20 09:27:44 +0300

    initalisation backenc web

---
+++ web-backend/src/test/java/backend/web/web_backend/WebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.web.web_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
