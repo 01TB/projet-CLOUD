b369c0d7435239306c5c4f972c96508464ec86b6
Tojooooo <tojonirina.rakotobe@yahoo.com>
2026-01-25 13:32:03 +0300

    dockerisation front + back

---
+++ projet/frontend-web/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+// src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
+
+const routes: Routes = [
+  { path: '', component: MapComponent },
+  { path: 'map', component: MapComponent },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
